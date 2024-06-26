aa8e47c009a9e88136269d27d3db45051946cd32
takuya <human.kagoshima.kurinotakuya@gmail.com>
2024-06-26 15:25:54 +0900

    プレイヤーコントローラーに少し変更を加えた

--- paintShot/Assets/Sclipts/PlayerController.cs
+++ paintShot/Assets/Sclipts/PlayerController.cs
@@ -4,28 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 10.0f; // 移動速度
+    public float lookSpeed = 2.0f; // 視点移動速度
+
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 60;
+        Application.targetFrameRate = 100;
+        Cursor.lockState = CursorLockMode.Locked; // マウスカーソルをロック
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        // プレイヤーの移動
+        float moveX = 0;
+        float moveZ = 0;
+        if (Input.GetKey(KeyCode.A))
         {
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                transform.Translate(-3, 0, 0);// 左
-            }
+            moveX -= moveSpeed * Time.deltaTime; // 左に移動
         }
-
+        if (Input.GetKey(KeyCode.D))
         {
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                transform.Translate(3, 0, 0);// 右
-            }
+            moveX += moveSpeed * Time.deltaTime; // 右に移動
         }
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveZ += moveSpeed * Time.deltaTime; // 前に移動
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveZ -= moveSpeed * Time.deltaTime; // 後ろに移動
+        }
+        transform.Translate(moveX, 0, moveZ);
+
+        // 視点の回転
+        float lookX = Input.GetAxis("Mouse X") * lookSpeed;
+        transform.Rotate(0, lookX, 0);
+
+        // カメラの上下回転を別のGameObject（例：カメラ自体）に適用する場合
     }
 }
